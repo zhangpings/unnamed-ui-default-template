2645b8525f4e7c57bb749b5078523bbb41da5b4e
Fan, Yi-Jie <fancn21th@aliyun.com>
2025-12-22 10:01:13 +0800

    👌 IMPROVE: ts error

--- runtime/useSmartVisionChatRuntime.ts
+++ runtime/useSmartVisionChatRuntime.ts
@@ -9,15 +9,20 @@ export const useSmartVisionChatRuntime = () => {
    * */
   const threadListAdapter = useSmartVisionThreadListAdapter();
 
+  /**
+   * ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️ 注意
+   * 这里必须返回一个新的ChatThreadRuntime实例
+   * 否则会导致线程列表和聊天线程之间的状态混乱
+   * 解释：assistant-ui 每一个聊天会话对应一个聊天的runtime
+   * ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+   * */
+
+  // https://www.assistant-ui.com/docs/runtimes/custom/custom-thread-list#how-it-works
+  // When the hook mounts it calls list() on your adapter, hydrates existing threads, and uses your runtime hook to spawn a runtime whenever a thread is opened. Creating a new conversation calls initialize(threadId) so you can create a record server-side and return the canonical remoteId.
+  const useRuntimeHook = () => useSmartVisionChatThreadRuntime();
+
   return useRemoteThreadListRuntime({
-    /**
-     * ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️ 注意
-     * 这里必须返回一个新的ChatThreadRuntime实例
-     * 否则会导致线程列表和聊天线程之间的状态混乱
-     * 解释：assistant-ui 每一个聊天会话对应一个聊天的runtime
-     * ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-     * */
-    runtimeHook: () => useSmartVisionChatThreadRuntime(),
+    runtimeHook: useRuntimeHook,
     adapter: threadListAdapter,
   });
 };
